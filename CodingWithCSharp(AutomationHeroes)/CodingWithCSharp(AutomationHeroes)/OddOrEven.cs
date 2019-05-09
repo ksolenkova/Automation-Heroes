1283ddd631eef7d94216755c1e10064f8c04b6df
ksolenkova <35841268+ksolenkova@users.noreply.github.com>
2019-05-09 16:27:16 +0300

    Initial commit

---
+++ CodingWithCSharp(AutomationHeroes)/CodingWithCSharp(AutomationHeroes)/OddOrEven.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CodingWithCSharp_AutomationHeroes_
+{
+    class OddOrEven
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to check if it's odd or even:");
+            var input = Console.ReadLine();
+            int digit = 0;
+            bool isParsed = int.TryParse(input, out digit);
+
+            if (isParsed)
+            {
+                if (digit % 2 == 0)
+                {
+                    Console.WriteLine($"Number {digit} is even.");
+                }
+                else
+                {
+                    Console.WriteLine($"Number {digit} is odd.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number!");
+            }
+        }
+    }
+}
