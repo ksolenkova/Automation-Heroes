07dae3c83e74c814d48c37a360c5ed2b7cf08290
krisi909 <krisi909@abv.bg>
2019-05-09 18:34:45 +0300

    Added two more console applications

---
+++ CodingWithCSharp(AutomationHeroes)/FirstThreeDigits/FirstThreeDigits.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FirstThreeDigits
+{
+    class FirstThreeDigits
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 3 real numbers:");
+            double sum = 0.0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                sum += input;
+            }
+
+            Console.WriteLine($"The sum of the entered digits is {sum}.");
+        }
+    }
+}
