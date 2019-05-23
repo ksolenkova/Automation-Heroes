904bc3a57c5ffeb2d77b0bf5d4fc4aea54a3c7ff
krisi909 <krisi909@abv.bg>
2019-05-23 10:49:48 +0300

    Adding some tasks from Programming Basics Online Exam - 20 and 21 April 2019

---
+++ CodingWithCSharp(AutomationHeroes)/EasterLunch/EasterLunch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EasterLunch
+{
+    class EasterLunch
+    {
+        static void Main(string[] args)
+        {
+            int easterBreadCount = int.Parse(Console.ReadLine());
+            int eggsBark = int.Parse(Console.ReadLine());
+            int cookies = int.Parse(Console.ReadLine());
+            
+            double priceForEasterBread = 3.20 * easterBreadCount;        
+            double priceForEggsBark = 4.35 * eggsBark;
+            double priceForCookies = 5.40 * cookies;
+            double priceForEggsPaint = 0.15 * 12 * eggsBark;
+            double totalLunchCost = priceForEasterBread + priceForEggsBark + priceForCookies + priceForEggsPaint;
+
+            Console.WriteLine($"{totalLunchCost:f2}");
+        }
+    }
+}
